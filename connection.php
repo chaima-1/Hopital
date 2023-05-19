29dc5cd975920c19504ca2d628cc03c533c5d5e3
chaima-1 <130319771+chaima-1@users.noreply.github.com>
2023-05-19 20:16:28 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("awseb-e-iwkp3jgvss-stack-awsebrdsdatabase-lmorl9inlrdx.cbz9gbamyctk.eu-north-1.rds.amazonaws.com","root","123456789","awseb-e-iwkp3jgvss-stack-awsebrdsdatabase-lmorl9inlrdx");
+    $database= new mysqli("awseb-e-iwkp3jgvss-stack-awsebrdsdatabase-lmorl9inlrdx.cbz9gbamyctk.eu-north-1.rds.amazonaws.com","root","123456789","ebdb");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
