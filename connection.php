264eb4161bb047b5e44a897ae29e9e07864ecf98
chaima-1 <130319771+chaima-1@users.noreply.github.com>
2023-05-19 19:06:20 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","","edoc");
+    $database= new mysqli("awseb-e-iwkp3jgvss-stack-awsebrdsdatabase-lmorl9inlrdx.cbz9gbamyctk.eu-north-1.rds.amazonaws.com","root","123456789","awseb-e-iwkp3jgvss-stack-awsebrdsdatabase-lmorl9inlrdx");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
